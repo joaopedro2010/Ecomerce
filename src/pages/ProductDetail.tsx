d375daddbe0e6142ffec17886973b9b7b4efacbf
joaopedro <joaopedro.gyerra@gmail.com>
2026-04-15 14:00:41 -0300

    fiz nada quase

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,4 +1,15 @@
 const ProductDetail =({...props})=>{
-    return<div {...props}>ProductDetail</div>
+    return<div {...props}>
+    
+       
+            <div>
+                <header>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRF8YkrnJ6porS4ntTa0MJmS_PnRnTa2o3Fg&s" alt=""/>
+                    <h1>FuFuni</h1>
+                </header>
+            </div>
+        
+
+    </div>
 }
 export default ProductDetail
